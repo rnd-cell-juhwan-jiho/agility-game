146f84b6de54b597bcdc78b0831b78512d92ec23
Hahn Jiho <ferrarijm@naver.com>
2022-01-13 12:18:19 +0900

    Resolve conflict

--- agility-game-view/src/App.js
+++ agility-game-view/src/App.js
@@ -1,12 +1,18 @@
 import './App.css'
-import {BrowserRouter as Router, Route, Routes} from 'react-router-dom'
+import {BrowserRouter as Router, Route, Routes, Navigate} from 'react-router-dom'
 import Header from './components/Header'
 import Login from './components/Login'
 import Games from './components/Games'
 import AuthProvider from './AuthProvider'
 import RequireAuth from './RequireAuth'
+// import {useContext} from 'react'
+// import {AuthContext} from './AuthProvider'
 
 function App() {
+
+  console.log("App()..")
+
+  // const {username} = useContext(AuthContext)
 
   return (
     <AuthProvider>
@@ -15,6 +21,7 @@ function App() {
         <hr/>
         <Router>
           <Routes>
+            <Route path="/" element={<Navigate to="/games"/>}/>
             <Route path="/login" element={<Login/>}/>
             <Route path="/games" element={
               <RequireAuth>
