91d640dc788cc000c687b82daad1254ee59aedac
Hahn Jiho <ferrarijm@naver.com>
2022-01-17 23:59:41 +0900

    Update game logic

--- agility-game-view/src/components/Games.js
+++ agility-game-view/src/components/Games.js
@@ -1,21 +1,19 @@
-import {useState, useEffect, useContext} from 'react'
+import {useState, useEffect} from 'react'
 import { useNavigate } from 'react-router-dom'
 import GameThumbnail from './GameThumbnail'
 import './Games.css'
-import {GameContext} from '../GameContextProvider'
 
-const test = [{
-    game_id: "123",
-    status: "VOTING",
-    size: 0
-}]
+// const test = [{
+//     game_id: "123",
+//     status: "VOTING",
+//     size: 0
+// }]
 
 const Games = (props) => {
 
     const navigate = useNavigate()
     const [games, setGames] = useState([])
     const url = "http://localhost:8080/games"
-    const {playing, setPlaying} = useContext(GameContext)
 
     useEffect(() => {
         fetchGames()
@@ -37,13 +35,7 @@ const Games = (props) => {
         })
     }
 
-    const fetchGamesTest = () => {
-        setGames(() => [...games, ...test]);
-    }
-
     const handleSubmit = e => {
-        e.preventDefault()
-
         navigate("/game/"+e.currentTarget.gameIdInput.value)
     }
 
